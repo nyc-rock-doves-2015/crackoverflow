d98a98950f54598fe9833d832f63e3448c330e0a
costolo <ben.costolo@gmail.com>
2015-04-30 18:11:12 -0400

    Add model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  has_many :comments
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  has_many :post_votes
+  belongs_to :user
+  has_many :answers, class_name: "Post", foreign_key: "question_id"
+  belongs_to :question, class_name: "Post"
 end
