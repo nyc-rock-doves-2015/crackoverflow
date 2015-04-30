2fb095fe9394659d1bc729cd1a4eea90ab059d2a
costolo <ben.costolo@gmail.com>
2015-04-30 18:04:28 -0400

    Add migrations for post, tags, post_votes, post_tags, and comments

--- db/migrate/20150430215620_create_users.rb
+++ db/migrate/20150430215620_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password_digest
-      t.integer :reputation
+      t.string :name, null: false, unique: true
+      t.string :password_digest, null: false
+      t.integer :reputation, default: 0
       t.string :profile_pic
-      
+
       t.timestamps
     end
   end
