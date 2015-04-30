73c6660e1addb34eea9815ed47c49aa732a2114b
costolo <ben.costolo@gmail.com>
2015-04-30 18:04:28 -0400

    Add migrations for post, tags, post_votes, post_tags, and comments

--- db/migrate/20150430215650_create_post_votes.rb
+++ db/migrate/20150430215650_create_post_votes.rb
@@ -1,7 +1,10 @@
 class CreatePostVotes < ActiveRecord::Migration
   def change
     create_table :post_votes do |t|
+      t.references :user
+      t.references :post
 
+      t.boolean :vote, null: false
       t.timestamps
     end
   end
--- db/migrate/20150430215801_create_post_tags.rb
+++ db/migrate/20150430215801_create_post_tags.rb
@@ -1,6 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration
   def change
     create_table :post_tags do |t|
+      t.references :post
+      t.references :tag
 
       t.timestamps
     end
