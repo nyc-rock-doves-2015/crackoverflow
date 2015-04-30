80790af16c43cd1e717c6c0713ec0daf4adc57fa
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-30 19:00:37 -0400

    first round of setting up resources for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :questions do
+    resources :answers, except: :new
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :tags, only: :create
+  resources :post_votes, only: [:create, :update]
+  resources :comments, only: [:create, :update, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
