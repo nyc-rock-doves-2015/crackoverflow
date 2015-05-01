6f4065552518303b1d133be2d6b6e011e6850184
Harry Park <imharrypark@gmail.com>
2015-05-01 18:26:23 -0400

    create comment that belongs to current_user and the answer it was posted to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,20 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+    @answer = Post.find(params[:answer_id])
   end
 
   def create
+    answer = Post.find(params[:answer_id])
+    question = answer.question
+
+    if current_user # must refactor
+      Comment.create(content: params[:comment][:content], user_id: current_user.id, post_id: params[:answer_id])
+    else
+      set_flash("You must be logged in")
+    end
+
+    redirect_to question_path(question)
   end
 
   def show
@@ -16,4 +28,9 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
