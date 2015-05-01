bdf5978cf53870b6ee50934063a9df3926dbffb1
costolo <ben.costolo@gmail.com>
2015-05-01 18:02:20 -0400

    initial ajax for comment posting

---
+++ app/assets/javascripts/answer.js
@@ -0,0 +1,14 @@
+$('document').ready(function () {
+  $('.new_answer').on("submit", function (event) {
+    event.preventDefault();
+    $target = $(event.target);
+    $.ajax({
+      url: $target.attr("action"),
+      type: "post",
+      data: $target.serialize()
+    }).done(function (response) {
+      $('#answer').prepend(response);
+      $('#answer_content').val('')
+    })
+  })
+})
