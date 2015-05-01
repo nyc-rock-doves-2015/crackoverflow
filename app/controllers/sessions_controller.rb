256484de9f56bafffa11e97aa81538148b81907d
Harry Park <imharrypark@gmail.com>
2015-05-01 13:29:52 -0400

    create destroy action for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
+      session_in!(@user)
       redirect_to root_path
     else
       set_flash "Incorrect username and/or password."
@@ -15,7 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session_out!
+    redirect_to root_path
   end
-
 
 end
