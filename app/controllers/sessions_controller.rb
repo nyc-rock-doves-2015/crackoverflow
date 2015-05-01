607527b0f81658f32939fbfb89b99b5d64c8d392
Harry Park <imharrypark@gmail.com>
2015-05-01 13:01:17 -0400

    create SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      set_flash "Incorrect username and/or password."
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+  end
+
+
+end
