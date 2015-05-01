a6137b7d0ec535c6c0ee7f89955cf5c79d660d88
Harry Park <imharrypark@gmail.com>
2015-05-01 13:51:15 -0400

    implement signup to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      session_in!(@user)
+      redirect_to root_path
+    else
+      set_flash "Username taken, or passwords do not match!"
+      redirect_to new_user_path
+    end
   end
 
   def show
@@ -19,4 +29,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
