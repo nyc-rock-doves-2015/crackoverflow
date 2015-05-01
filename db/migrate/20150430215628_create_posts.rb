b3bfc09d1a0fe346def34c79aa5ddefb4ca6be3f
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-01 08:07:55 -0400

    seed data more robust, edit post title can be null

--- db/migrate/20150430215628_create_posts.rb
+++ db/migrate/20150430215628_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, null: false
+      t.string :title
       t.text :content, null: false
       t.integer :reputation, default: 0
       t.references :user
