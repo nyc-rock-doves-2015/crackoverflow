76052ed51519c9a3e1705692fe6fcbbfe17866bb
costolo <ben.costolo@gmail.com>
2015-05-02 12:19:18 -0400

    add search bar functionality

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,6 @@
 $(document).on('ready page:load', function () {
-  $('.typeahead').typeahead([
-    {
-      prefetch: 'http://localhost:3000/suggestion.json'
-    }
-  ]);
+  $('#search-bar .typeahead').typeahead({
+    prefetch: 'http://localhost:3000/suggestion.json',
+    hint: true
+  });
 });
