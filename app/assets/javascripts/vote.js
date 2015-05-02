3091b979295136a56c667d8bc9ce810d9f48e398
costolo <ben.costolo@gmail.com>
2015-05-02 16:34:26 -0400

    Add ajax call for real time reputation updating in vote.js

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,3 +1,15 @@
-$(document).on('ready page:load', function() {
+$(document).on('ready page:load', function () {
+  $('.vote-link').click(function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
-})
+    var $target = $(event.target).parent();
+    var $reputation = $target.parent().children('.reputation')
+    $.ajax({
+      url: $target.attr('href'),
+      type: 'post'
+    }).done(function (response) { 
+      $reputation.html(response)
+    });
+  });
+});
