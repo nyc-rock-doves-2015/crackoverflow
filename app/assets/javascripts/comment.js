63310c8b0761e488af7939505ab11a8b96add7dd
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-02 16:38:07 -0400

    add commenting on questions

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,7 +2,7 @@ $(document).on('ready page:load', function() {
 
   $('.comment-new-form').hide()
 
-  $('.answer-container').on('click', '.comment-add-link', function(event) {
+  $('.post-container').on('click', '.comment-add-link', function(event) {
     event.preventDefault();
     event.stopPropagation();
 
@@ -12,17 +12,17 @@ $(document).on('ready page:load', function() {
 
   })
 
-  $('.answer-container').on('submit', '.comment-new-form', function(event) {
+  $('.post-container').on('submit', '.comment-new-form', function(event) {
     event.preventDefault();
 
     var $target = $(event.target);
-    var $answerId = $target.closest('.answer-container').children('.post-id').html();
+    var $postId = $target.closest('.post-container').children('.post-id').html();
     var $commentContainer = $target.closest('.comment-container')
 
     $.ajax({
       url: '/comments',
       type: "post",
-      data: $target.serialize() + "&post_id=" + $answerId
+      data: $target.serialize() + "&post_id=" + $postId
     }).done(function(response) {
       $commentContainer.children('.comment-list').append(response);
       $commentContainer.find('.comment-add-link').show();
