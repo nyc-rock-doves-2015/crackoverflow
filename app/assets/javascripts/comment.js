9f9b47346441d79315784c040df617c292cd103d
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-02 15:03:13 -0400

    start javascript to show comment form on click

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,3 +1,13 @@
 $(document).on('ready page:load', function() {
 
+  $('.answer-container').on('click', '.comment-add-link', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var $target = $(event.target)
+    $target.hide();
+    // $target.closest('.comment-list').append
+
+  })
+
 })
