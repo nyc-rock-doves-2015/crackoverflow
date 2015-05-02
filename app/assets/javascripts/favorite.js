ec4d135440a1720bd7db22f6fd190cd8e1251d52
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-01 21:59:33 -0400

    preventDefault for favorite working

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-  
+$(document).on('ready page:load', function() {
+
+  $('.favorite-toggle').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $target = $(event.target)
+
+    // $.ajax({
+    //   url: '/favorites', 
+    //   type: "POST",
+    //   data: {question_id: }
+
+    $target.hide();
+  })
+
 })
