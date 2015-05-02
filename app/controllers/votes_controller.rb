325ce55ec638b7444f0b3aa28af8bcfdc83011b2
costolo <ben.costolo@gmail.com>
2015-05-02 13:24:59 -0400

    Add logic for changing votes, but commented it out due to how unresponsive it causes the site to be

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,7 @@ class VotesController < ApplicationController
       create_vote(true)
       current_post.update_reputation("up")
     end
+    # change_vote("up")
     redirect_to question_path(current_post)
   end
 
@@ -13,12 +14,27 @@ class VotesController < ApplicationController
       create_vote(false)
       current_post.update_reputation("down")
     end
+    # change_vote("down")
     redirect_to question_path(current_post)
   end
 
-  def current_vote
-    PostVote.where(user_id: current_user.id, post_id: current_post.id)
-  end
+  # def current_vote
+  #   PostVote.find_by(user_id: current_user.id, post_id: current_post.id)
+  # end
+
+  # def change_vote(dir)
+  #   if dir == "down" && current_vote.vote
+  #     2.times do
+  #       current_vote.post.update_reputation("down")
+  #     end
+  #     current_user.post_votes.find_by(post_id: current_post.id).update(vote: false)
+  #   elsif dir == "up" && !current_vote.vote 
+  #     2.times do
+  #       current_vote.post.update_reputation("up")
+  #     end
+  #     current_user.post_votes.find_by(post_id: current_post.id).update(vote: true)
+  #   end
+  # end
 
   def vote_doesnt_exist?
     if PostVote.where(user_id: current_user.id, post_id: current_post.id).empty?
