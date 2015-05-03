dfd8ff387eea4863f98017623e0dba5be87c3df0
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-03 16:13:42 -0400

    add user handling for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,23 +8,46 @@ class CommentsController < ApplicationController
 
     post = Post.find(params[:post_id])
 
-    if current_user
-      comment = Comment.create(content: params[:comment][:content], user_id: current_user.id, post: post)
-
-      if request.xhr?
+    if request.xhr?
+      if current_user
+        comment = Comment.create(content: params[:comment][:content], user_id: current_user.id, post: post)
         render partial: 'show', locals: {comment: comment}
       else
-        if post.question_id
-          redirect_to question_path(post.question)
-        else
-          redirect_to question_path(post)
-        end
+        set_flash('You must be logged in')
+        head :unauthorized
       end
     else
-      set_flash('You must be logged in')
-      redirect_to new_sessions_path
+      if current_user
+        comment = Comment.create(content: params[:comment][:content], user_id: current_user.id, post: post)
+        redirect_to question_path(post.question) if post.question_id
+        redirect_to question_path(post)
+      else
+        set_flash('You must be logged in')
+        redirect_to new_sessions_path
+      end
     end
   end
+
+  # if current_user
+  #   comment = Comment.create(content: params[:comment][:content], user_id: current_user.id, post: post)
+
+  #   if request.xhr?
+  #     render partial: 'show', locals: {comment: comment}
+  #   else
+  #     if post.question_id
+  #       redirect_to question_path(post.question)
+  #     else
+  #       redirect_to question_path(post)
+  #     end
+  #   end
+  # else
+  #   set_flash('You must be logged in')
+  #   if request.xhr?
+  #     render partial: '/sessions/fail'
+  #   else
+  #     redirect_to new_sessions_path
+  #   end
+  # end
 
   def show
   end
