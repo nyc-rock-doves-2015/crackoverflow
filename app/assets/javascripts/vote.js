b278a1a8279e3c5ec444d50ad2251d04f9438f5e
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-03 12:46:37 -0400

    Update ajax call to pass back post_id and direction

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -4,11 +4,15 @@ $(document).on('ready page:load', function () {
     event.stopPropagation();
 
     var $target = $(event.target).parent();
-    var $reputation = $target.parent().children('.reputation')
-    var $userReputation = $target.closest('.post-container').find('.user-reputation')
+    var $reputation = $target.parent().children('.reputation');
+    var $userReputation = $target.closest('.post-container').find('.user-reputation');
+    var $postId = $target.closest('.post-container').find('.post-id').html();
+    var $voteDecision = $target.attr('id') === "upvote-link"
+
     $.ajax({
       url: $target.attr('href'),
-      type: 'post'
+      type: 'post',
+      data: {post_id: $postId, vote: $voteDecision }
     }).done(function (response) {
       var resArray = response.split("|")
       $reputation.html(resArray[0])
