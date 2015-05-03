d2db8cc23c141313b6a8b4d14b0642a541ee71e5
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-03 16:14:44 -0400

    remove comments from comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,27 +28,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # if current_user
-  #   comment = Comment.create(content: params[:comment][:content], user_id: current_user.id, post: post)
-
-  #   if request.xhr?
-  #     render partial: 'show', locals: {comment: comment}
-  #   else
-  #     if post.question_id
-  #       redirect_to question_path(post.question)
-  #     else
-  #       redirect_to question_path(post)
-  #     end
-  #   end
-  # else
-  #   set_flash('You must be logged in')
-  #   if request.xhr?
-  #     render partial: '/sessions/fail'
-  #   else
-  #     redirect_to new_sessions_path
-  #   end
-  # end
-
   def show
   end
 
