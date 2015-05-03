d8250241410e26b38763f8d35424d64bc79cc914
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-03 12:48:27 -0400

    logic for handling users trying to vote multiple times

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,11 +9,35 @@ class VotesController < ApplicationController
   end
 
   def downvote
-    if current_user && vote_doesnt_exist? 
+    if current_user && vote_doesnt_exist?
       create_vote(false)
       current_post.update_reputation("down")
     end
     return_reputation
+  end
+
+  def create_or_update
+    if current_user
+      p params
+      post = Post.find(params[:post_id])
+      vote = PostVote.find_or_create_by(post: post, user: current_user)
+      vote_boolean = to_boolean(params[:vote])
+
+      if vote.vote.nil?
+        vote.update(vote: vote_boolean)
+        vote_boolean == true ? (dir = "up") : (dir = "down")
+        post.update_reputation(dir)
+      elsif vote.vote != vote_boolean
+        vote.update(vote: vote_boolean)
+        vote_boolean == true ? (dir = "up") : (dir = "down")
+        2.times {post.update_reputation(dir)}
+      elsif vote.vote == vote_boolean
+        vote.update(vote: nil)
+        vote_boolean == true ? (dir = "down") : (dir = "up")
+        post.update_reputation(dir)
+      end
+    end
+    render partial: 'votes/reputation', layout: false, locals: {post: post}
   end
 
   def return_reputation
