9a33e04ee42dacfd5cdaed2764df5e6e0b7b7350
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-03 15:36:26 -0400

    add user validation for certain functions

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -11,9 +11,13 @@ $(document).on('ready page:load', function() {
       type: "POST",
       data: {question_id: $('.question-id').html()}
     }).done(function(response) {
-      $target.removeClass('fa-star-o');
-      $target.addClass('fa-star');
-      $('.favorite-count').html(response);
+      if (response != "signin") {
+        $target.removeClass('fa-star-o');
+        $target.addClass('fa-star');
+        $('.favorite-count').html(response);
+      } else {
+        window.location.replace("/sessions/new")
+      }
     })
   })
 
